1bfcb187f567cf6996bab37bcc109fee591a7a5c
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-03-30 23:01:11 -0500

    Now parsing the string to separate date and time values

--- WorldTimeZoneConverter.rb
+++ WorldTimeZoneConverter.rb
@@ -1,11 +1,17 @@
 require 'httparty'
 require 'json'
+require 'date'
 
 # Insert desired area and location here
 response = HTTParty.get('http://worldtimeapi.org/api/timezone/Europe/London')
+
 time_data = JSON.parse(response.body)
 
 # Extract the current date and time
 datetime = time_data["datetime"]
 
+# Parse the datetime string and format it
+datetime = DateTime.parse(datetime).strftime("%Y-%m-%d %H:%M:%S")
+
+# Print the current date and time in the specified format
 puts "The current time in Europe/London is #{datetime}"
